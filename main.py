a5d71fac84c25cc38ad14aba4973e0ea06533f27
dserver <ppdavisito89@gmail.com>
2025-04-30 17:46:54 -0400

    Stuck in Convert Process

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from importlib.metadata import pass_none
-
 import PyPDF2
 import pyttsx3
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, url_for
 from flask_wtf import FlaskForm
+from requests import session
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
@@ -12,6 +12,7 @@ from wtforms.validators import InputRequired
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
+upload_paths = {}
 
 # Form Class
 class UploadFileForm(FlaskForm):
@@ -43,8 +44,22 @@ def home():
         filename = secure_filename(file.filename)
         save_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], filename) # Then save the file
         file.save(save_path)
+        #Store in session
+        upload_paths['latest'] = save_path
+
         flash("File uploaded successfully, please proceed to convert it.  ")
     return render_template('index.html', form=form, form2=form2)
 
+@app.route('/convert', methods=['POST'])
+def convert(path):
+    form2 = ConvertFileForm()
+    if form2.validate_on_submit():
+        with open(path, 'r') as f:
+            content = f.read()
+            print(content)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
+    convert(upload_paths)
